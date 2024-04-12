e1f737be0bc8d623a264a0ff808ebeaf45dce377
Leandro T. C. Melo <ltcmelo@gmail.com>
2024-04-12 10:37:48 -0300

    try async and imperative style

--- index.js
+++ index.js
@@ -3,38 +3,33 @@ const fs = require('fs');
 const lodash = require('lodash');
 const moment = require('moment');
 
-function fetchAndDisplayUserData() {
-    axios.get('https://randomuser.me/api/?results=5')
-        .then(function (response) {
-            const users = response.data.results;
-            const userData = [];
+async function fetchAndDisplayUserData() {
+    try {
+        // Fetch data from the API
+        const response = await axios.get('https://randomuser.me/api/?results=5');
+        const users = response.data.results;
+        const userData = [];
 
-            console.log("List of Users:");
-            for (let i = 0; i < users.length; i++) {
-                const user = users[i];
-                const userInfo = {
-                    firstName: lodash.get(user, 'name.first', 'No first name'),
-                    lastName: lodash.get(user, 'name.last', 'No last name'),
-                    registrationDate: moment(lodash.get(user, 'registered.date')).format('MMMM Do YYYY, h:mm:ss a')
-                };
-                console.log(`${i + 1}: ${userInfo.firstName} ${userInfo.lastName} - Registered on: ${userInfo.registrationDate}`);
-                userData.push(userInfo);
-            }
+        console.log("List of Users:");
+        for (let i = 0; i < users.length; i++) {
+            const user = users[i];
+            const userInfo = {
+                firstName: lodash.get(user, 'name.first', 'No first name'),
+                lastName: lodash.get(user, 'name.last', 'No last name'),
+                registrationDate: moment(lodash.get(user, 'registered.date')).format('MMMM Do YYYY, h:mm:ss a')
+            };
 
-            // Save to JSON file
-            const jsonContent = JSON.stringify(userData, null, 2);
-            fs.writeFile('userData.json', jsonContent, 'utf8', (err) => {
-                if (err) {
-                    console.log("An error occurred while writing JSON Object to File.");
-                    return console.log(err);
-                }
+            console.log(`${i + 1}: ${userInfo.firstName} ${userInfo.lastName} - Registered on: ${userInfo.registrationDate}`);
+            userData.push(userInfo);
+        }
 
-                console.log("JSON file has been saved.");
-            });
-        })
-        .catch(function (error) {
-            console.error('Failed to fetch data:', error);
-        });
+        // Save to JSON file
+        const jsonContent = JSON.stringify(userData, null, 2);
+        fs.writeFileSync('userData.json', jsonContent, 'utf8');
+        console.log("JSON file has been saved.");
+    } catch (error) {
+        console.error('Failed to fetch data:', error);
+    }
 }
 
 // Run the function
